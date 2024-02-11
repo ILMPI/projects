100aeec44c2b4431071c6c62cc3595281e017112
ILMPI <eguzkia6@gmail.com>
2024-02-11 21:29:00 +0100

    adding a blog component and changes

--- a5_blog/src/app/blog/blog.component.ts
+++ a5_blog/src/app/blog/blog.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { IPost } from '../interfaces/ipost.interface';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.css'
+  styleUrl: './blog.component.css',
 })
 export class BlogComponent {
+  posts: IPost[] = [
+    {
+      title: 'Little Robot',
+      imageUrl:
+        'https://cdn.pixabay.com/photo/2023/12/14/16/39/robot-8449206_960_720.jpg',
+      content: 'This is the cute image of a little robot companion.',
+      date: new Date(),
+    },
+    {
+      title: 'The white bear',
+      imageUrl:
+        'https://cdn.pixabay.com/photo/2020/09/04/20/09/cartoon-5544856_960_720.jpg',
+      content: 'This is the white bear criminal.',
+      date: new Date(),
+    },
+  ];
 
+  addPost(title: string, imageUrl: string, content: string): void {}
 }
--- a5_blog/src/app/interfaces/ipost.interface.ts
+++ a5_blog/src/app/interfaces/ipost.interface.ts
@@ -1,6 +1,6 @@
 export interface IPost {
   title: string;
   imageUrl: string;
-  text: string;
-  date: string; // or Date for actual Date objects
+  content: string;
+  date: Date;
 }
