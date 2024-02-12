520b51abd4f5535e3e106f004170c78e1431ee5e
ILMPI <eguzkia6@gmail.com>
2024-02-12 05:16:41 +0100

    little changes

--- a5_blog/src/app/components/blog/blog.component.ts
+++ a5_blog/src/app/components/blog/blog.component.ts
@@ -15,18 +15,20 @@ export class BlogComponent {
   newPost: IPost = { title: '', imageUrl: '', content: '', date: '' };
   posts: IPost[] = [
     {
-      title: 'Little Robot',
+      title: 'Little robot Gizmo and Whiskers',
       imageUrl:
         'https://cdn.pixabay.com/photo/2023/12/14/16/39/robot-8449206_960_720.jpg',
-      content: 'This is the cute image of a little robot companion.',
-      date: '12/02/2024',
+      content:
+        'In the heart of a cozy home, an adorable little robot named Gizmo embarked on a mission to tidy up. With its big, expressive eyes focused and tiny arms at the ready, Gizmo set about organizing a jumble of toys. But each time Gizmo sorted a toy, its playful pet cat, Whiskers, would pounce, scattering them anew. The scene was a comical dance of order and chaos, with Gizmo determinedly picking up toys and Whiskers gleefully undoing all the hard work. In the end, Gizmo decided that playing with Whiskers was much more fun than cleaning up, turning a chore into a delightful game for two.',
+      date: '02/12/2024',
     },
     {
-      title: 'The white bear',
+      title: 'The Case of the Polar Intruder',
       imageUrl:
         'https://cdn.pixabay.com/photo/2020/09/04/20/09/cartoon-5544856_960_720.jpg',
-      content: 'This is the white bear criminal.',
-      date: '13/02/2024',
+      content:
+        "In the cozy town of Snowy Peaks, the local bakery's famous chocolate cakes started mysteriously disappearing. The prime suspect? A cheeky polar bear seen loitering nearby. Enter Detective Paws, a clever little polar bear with a knack for sweets and mysteries. Following a trail of chocolate crumbs and snowy paw prints, Detective Paws discovered the real thief: a family of mischievous raccoons using the polar bear's naps as their chance to swipe the cakes. With a gentle growl and a wag of his fluffy tail, Paws forgave the raccoons, and together, they shared the remaining cake, turning a caper into a celebration.",
+      date: '02/10/2024',
     },
   ];
   imageError(event: any) {
@@ -41,7 +43,7 @@ export class BlogComponent {
       this.newPost.content &&
       this.newPost.date
     ) {
-      this.posts.push({ ...this.newPost });
+      this.posts.unshift({ ...this.newPost }); //!!!!!!!!!!!!!!
       this.newPost = { title: '', imageUrl: '', content: '', date: '' };
     } else {
       alert('Make sure you fill in all required fields.');
